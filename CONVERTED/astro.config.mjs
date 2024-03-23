2c7cea21b451223877a41d69e476ef812a6bd3ec
rr-h <137926547+rr-h@users.noreply.github.com>
2024-03-23 14:25:06 +0000

    scss

--- CONVERTED/astro.config.mjs
+++ CONVERTED/astro.config.mjs
@@ -1,6 +1,8 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import markdown from '@astrojs/markdown-remark';
+import svelte from '@astrojs/svelte';
 
 export default defineConfig({
-  integrations: [markdown()],
+  integrations: [markdown(), svelte()],
 });
