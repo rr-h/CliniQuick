5fb17f1803be4f671c32350c0b9ae3b1bcc1be8a
rr-h <reuther.hols@gmail.com>
2024-03-24 14:29:36 +0000

    module

---
+++ NEW/app/cliniquick-ui/index.d.ts
@@ -0,0 +1,16 @@
+declare module "@reuther-hols/cliniquick-ui" {
+	type DefaultColorProps = {
+		color?: string;
+	};
+
+	export function Button(props: DefaultColorProps): any;
+	export function ActualButton(props: DefaultColorProps): any;
+	export function Card(props: DefaultColorProps): any;
+	export function Pill(props: DefaultColorProps): any;
+
+	type BrutalConfiguration = {
+		colors: string[];
+	};
+
+	export function Config(): BrutalConfiguration;
+}
